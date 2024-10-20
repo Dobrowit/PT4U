be37ba65cae49aacec1d97c9f3ea1dd4c4f6dab6
Dobrowit <radekcz@gmail.com>
2024-10-20 08:02:52 +0200

    Aktualizacja

--- notihand.py
+++ notihand.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 
-import glib
+import gi
+from gi.repository import GLib
 import dbus
 from dbus.mainloop.glib import DBusGMainLoop
 
-def notifications(bus, message):
-    print [arg for arg in message.get_args_list()]
+def print_notification(bus, message):
+  keys = ["app_name", "replaces_id", "app_icon", "summary",
+          "body", "actions", "hints", "expire_timeout"]
+  args = message.get_args_list()
+  if len(args) == 8:
+    notification = dict([(keys[i], args[i]) for i in range(8)])
+    print(notification["summary"], notification["body"])
 
-DBusGMainLoop(set_as_default=True)
+loop = DBusGMainLoop(set_as_default=True)
+session_bus = dbus.SessionBus()
+session_bus.add_match_string("type='method_call',interface='org.freedesktop.Notifications',member='Notify',eavesdrop=true")
+session_bus.add_message_filter(print_notification)
 
-bus = dbus.SessionBus()
-bus.add_match_string_non_blocking("eavesdrop=true, interface='org.freedesktop.Notifications', member='Notify'")
-bus.add_message_filter(notifications)
-
-mainloop = glib.MainLoop()
-mainloop.run()
+GLib.MainLoop().run()
